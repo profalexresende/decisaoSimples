e8627ad89ed3c04e901c4406d786953fec8de031
Alex Sander Resende de Deus <prof.alexresende@gmail.com>
2021-09-20 16:16:48 -0300

    Desvio Condicional Simples

---
+++ idadeJogador/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace idadeJogador
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, RoutedEventArgs e)
+        {
+            int idade;
+            idade = Convert.ToInt32(txtIdade.Text);
+
+            //Iniciando a condição
+            if (idade >= 12)
+            {
+                //BLOCO PARA CONDIÇÃO VERDADEIRA
+                MessageBox.Show("Você pode jogar!", "ONG",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+    }
+}
